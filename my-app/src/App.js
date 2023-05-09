723912082cc99d232e1c0bc15f700cc4aef76576
Charlie <crc_185@yahoo.com>
2023-05-08 20:55:12 -0700

    fixed-subfolder

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from './Components/Navbar';
+import { useState, useEffect } from "react"
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <body>
+        
+      </body>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/Components/Navbar.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+      <header>
+        <div>
+
+        </div>
+        <div>
+          <ul>
+            <a>About Me</a>
+          </ul>
+          <ul>
+            <a>What I've Worked On</a>
+          </ul>
+          <ul>
+            <a>Skills</a>
+          </ul>
+          <ul>
+            <a>Resume</a>
+          </ul>
+          <ul>
+            <a></a>
+          </ul>
+        </div>
+      </header>
+    </div>
+  )
+}
+
+export default Navbar
