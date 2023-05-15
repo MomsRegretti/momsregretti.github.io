12d438892bd532e5e502c9ef61441a63737251b0
Charlie <crc_185@yahoo.com>
2023-05-15 00:24:23 -0700

    Technology logos added

--- my-app/src/Components/Technologies.js
+++ my-app/src/Components/Technologies.js
@@ -1,11 +1,20 @@
 import React from 'react'
-
 function Technologies() {
   return (
     <div>
       <h1>These are the technologies I've learned and implemented in my projects/learning!</h1>
       <div>
-        <img src='../' alt='javascript'></img>
+        <img className='technology' src='https://cdn.cdnlogo.com/logos/j/69/javascript.svg' alt='javascript'/>
+        <img className='technology' src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/CSS3_logo.svg/240px-CSS3_logo.svg.png' alt=''/>
+        <img className='technology' src='https://cdn.worldvectorlogo.com/logos/html-1.svg' alt=''/>
+        <img className='technology' src='https://upload.wikimedia.org/wikipedia/commons/3/33/Figma-logo.svg' alt=''/>
+        <img className='technology' src='https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1869px-Python-logo-notext.svg.png' alt=''/>
+        <img className='technology' src='https://miro.medium.com/v2/resize:fit:800/1*eRErB-NQYgwF52eUUK_kkQ.png' alt=''/>
+        <img className='technology' src='https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/R_logo.svg/1200px-R_logo.svg.png' alt=''/>
+        <img className='technology' src='https://seeklogo.com/images/N/nodejs-logo-FBE122E377-seeklogo.com.png' alt=''/>
+        <img className='technology' src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png' alt=''/>
+        <img className='technology' src='https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg' alt=''/>
+
       </div>
     </div>
   )
