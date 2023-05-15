400f9db55ebe9c65e2121e9ea7af83dca3e58899
Charlie <crc_185@yahoo.com>
2023-05-14 23:58:42 -0700

    Technology section

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,12 +3,14 @@ import Navbar from './Components/Navbar';
 // import AboutMe from './Components/AboutMe';
 import Home from './Components/Home';
 // import { useState, useEffect } from "react"
+import Technologies from './Components/Technologies'
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <body>
         <Home/>
+        <Technologies/>
       </body>
     </div>
   );
--- my-app/src/Components/Technologies.js
+++ my-app/src/Components/Technologies.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 function Technologies() {
   return (
-    <div>Technologies</div>
+    <div>
+      <h1>These are the technologies I've learned and implemented in my projects/learning!</h1>
+      <div>
+        <img src='../' alt='javascript'></img>
+      </div>
+    </div>
   )
 }
 
