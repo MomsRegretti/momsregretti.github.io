8d609684d0bacb7485c99ab7099f8e5dbe90c1dd
Charlie <crc_185@yahoo.com>
2023-05-23 22:50:04 -0700

    imported routers into file

--- my-app/src/Components/Home.js
+++ my-app/src/Components/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Home() {
     return (
         <div style={{ backgroundColor: "#121212", minHeight: '900px', display: 'flex', direction: 'row', margin: 'auto' }}>
-            <div className = 'fox' style={{ margin: 'auto', width: '50%', background:'#373737' }} id='pic'>
+            <div className = 'imageCard' style={{}} id='pic'>
                 <img style={{ margin: 'auto', height: '33%', width: '33%' }} alt='Charlie Rojas' src='https://scontent-sjc3-1.xx.fbcdn.net/v/t1.18169-9/10410413_650047695099302_2666444969946121164_n.jpg?stp=dst-jpg_p180x540&_nc_cat=101&ccb=1-7&_nc_sid=2c4854&_nc_ohc=F8LI4HREuDUAX9pBwvy&_nc_ht=scontent-sjc3-1.xx&oh=00_AfA9QjzWge7oWoP-kTyzEbNuKrOXv4ZIsUduoCw-pDo7AQ&oe=648423A7' />
                 <h2>This is me enjoying myself</h2>
             </div>
