9298699cf9016a34fde1b1351af57233bd686450
Charlie <crc_185@yahoo.com>
2024-01-12 17:09:46 -0800

    added image to memory and stylized it

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import ye from '/home/potus/projects/MomsRegretti.github.io/src/IMG_0014.JPG'
 function AboutMe({aboutMeRef}) {
     // const aboutMeRef = useRef(null)
     return (
         <div ref={aboutMeRef} id='aboutme' style={{ backgroundColor: "#121212", minHeight: '900px', display: 'flex', direction: 'row', margin: 'auto' }}>
             <div className = 'imageCard' style={{}} id='pic'>
-                <img style={{ margin: 'auto', height: '25rem', width: '45rem', borderRadius:'15px'}} alt='Charlie Rojas' src='https://scontent-sjc3-1.xx.fbcdn.net/v/t1.18169-9/10410413_650047695099302_2666444969946121164_n.jpg?stp=dst-jpg_p180x540&_nc_cat=101&ccb=1-7&_nc_sid=2c4854&_nc_ohc=F8LI4HREuDUAX9pBwvy&_nc_ht=scontent-sjc3-1.xx&oh=00_AfA9QjzWge7oWoP-kTyzEbNuKrOXv4ZIsUduoCw-pDo7AQ&oe=648423A7' />
+                <img style={{ margin: 'auto', height: '25rem', borderRadius:'15px', padding:'0px 10px'}} alt='Charlie Rojas' src = {ye} />
                 <h2>This is me enjoying myself</h2>
             </div>
             <div style={{ justifySelf: 'center', width: '50%', display: 'grid',marginRight: '150px' }} id='bio'>
