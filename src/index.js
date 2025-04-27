c56e9058b4df2c4f97a6a195428323258c575870
Charlie Rojas <100166462+MomsRegretti@users.noreply.github.com>
2025-04-27 20:02:13 +0000

    build script change

--- src/index.js
+++ src/index.js
@@ -5,15 +5,11 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(
-  <>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </>
-
+ReactDOM.render(
+  <BrowserRouter basename="/momsregretti.github.io">
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
